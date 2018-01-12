d48d3aea44f81f96b49c19fe4de9e3d016c69f98
Gadzhidadaev Gadzhi <gadzhi7.7.7@mail.ru>
2018-01-12 09:26:58 +0300

    connected second and first

--- js/script.js
+++ js/script.js
@@ -1,104 +1,40 @@
-/*var hwSlideSpeed = 200;
-var hwTimeOut = 300;
-var hwNeedLinks = true;
-
-
-$(document).ready(function(e) {
-    $('.slide').hide().eq(0).show();
-    var slideNum = 0;
-    var slideTime;
-    var slideCount = $(".slide").length;
-    var animSlide = function(arrow){
-        clearTimeout(slideTime);
-        $('.slide').eq(slideNum).fadeOut(hwSlideSpeed);
-		
-		if(slideNum == (slideCount-1)){slideNum=0;}
-		
-        if(arrow == "next"){
-            if(slideNum == (slideCount-1)){slideNum=0;}
-            else{slideNum++}
-            }
-        else if(arrow == "prew")
-        {
-            if(slideNum == 0){slideNum = slideCount - 1;}
-            else{slideNum-=1}
-        }
-        else{
-            slideNum = arrow;
-            }
-    }
-		
-	if(hwNeedLinks){
-		var $linkArrow = $('<a id="prewbutton" href="#"><</a><a id="nextbutton" href="#">></a>')
-		.prependTo('.slideshow');      
-		$('#nextbutton').click(function(){
-			animSlide("next");
- 
-		})
-		$('#prewbutton').click(function(){
-			animSlide("prew");
-        })
-	}
-    var $adderSpan = '';
-
-    var pause = false;
-    var rotator = function(){
-    if(!pause){slideTime = setTimeout(function(){animSlide('next')}, hwTimeOut);}
-            }
-    $('#slider-wrap').hover(
-        function(){clearTimeout(slideTime); pause = true;},
-        function(){pause = false; rotator();
-        });
-    rotator();
+window.onload = function () {
+	
+	
+$('.slide-button').click(function (event) {
+	event.preventDefault();
 });
 
-*/
-
-
-window.onload = function () {
-    
+	
+	
     new Slider({
-        images: '.slideshow1 .slide',
+        images: '.slideshow1 .slide1',
         btnPrev: '#prewbutton1',
         btnNext: '#nextbutton1',
-        auto: false
-    });
-    
-	/*new Slider({
-        images: '.gallery-2 img',
-        btnPrev: '.gallery-2 .buttons .prev',
-        btnNext: '.gallery-2 .buttons .next',
         auto: true,
-        rate: 2000
-    });*/
+		rate: 5000
+    });
+	
 }
-
 
 function Slider(obj) {
     this.images = document.querySelectorAll(obj.images);
-		
-	this.auto = obj.auto;
-		
+	this.auto = obj.auto;	
 	this.btnPrev = obj.btnPrev;
 	this.btnNext = obj.btnNext;
-    
     this.rate = obj.rate || 1000;
-	
 	var i = 0;
-    
     var slider = this;
-
 	this.prev = function () {
 		slider.images[i].classList.remove('showed');
 		i--;
-
+		
 		if (i < 0) {
 			i = slider.images.length - 1;
 		}
 
 		slider.images[i].classList.add('showed');
 	}
-
 	this.next = function () {
 		slider.images[i].classList.remove('showed');
 		i++;
@@ -109,12 +45,99 @@ function Slider(obj) {
 
 		slider.images[i].classList.add('showed');
 	}
-	
     document.querySelector(slider.btnPrev).onclick = slider.prev;
-    document.querySelector(slider.btnNext).onclick = slider.next;
-		
+    document.querySelector(slider.btnNext).onclick = slider.next;	
 	if(slider.auto)	{
         setInterval(slider.next, slider.rate);
     }
 }
 
+			
+jQuery(document).ready(function(){
+ function htmSlider(){
+  var slideWrap = jQuery('.slideshow2__inner');
+  var nextLink = jQuery('#nextbutton2');
+  var prevLink = jQuery('#prewbutton2');
+  var slideWidth = jQuery('.slide2').outerWidth();
+  var scrollSlider = slideWrap.position().left - slideWidth * 4;
+		
+  nextLink.click(function(){
+   if(!slideWrap.is(':animated')) {
+    slideWrap.animate({left: scrollSlider}, 500, function(){
+     slideWrap
+      .find('.slide2:first')
+      .appendTo(slideWrap)
+      .parent()
+      .css({'left': 0});
+    });
+   }
+  });
+
+  prevLink.click(function(){
+   if(!slideWrap.is(':animated')) {
+    slideWrap
+     .css({'left': scrollSlider})
+     .find('.slide2:last')
+     .prependTo(slideWrap)
+     .parent()
+     .animate({left: 0}, 500);
+   }
+  });
+
+};
+ 
+ htmSlider();
+});
+
+
+
+			
+jQuery(document).ready(function(){
+ function htmSlider1(){
+  var slideWrap1 = jQuery('.slideshow3__inner');
+  var nextLink1 = jQuery('#nextbutton3');
+  var prevLink1 = jQuery('#prewbutton3');
+  var slideWidth1 = jQuery('.slide3').outerWidth();
+  var scrollSlider1 = slideWrap1.position().left - slideWidth1 * 4;
+		
+  nextLink1.click(function(){
+   if(!slideWrap1.is(':animated')) {
+    slideWrap1.animate({left: scrollSlider1}, 500, function(){
+     slideWrap1
+      .find('.slide3:first')
+      .appendTo(slideWrap1)
+      .parent()
+      .css({'left': 0});
+    });
+   }
+  });
+
+  prevLink1.click(function(){
+   if(!slideWrap1.is(':animated')) {
+    slideWrap1
+     .css({'left': scrollSlider1})
+     .find('.slide3:last')
+     .prependTo(slideWrap1)
+     .parent()
+     .animate({left: 0}, 500);
+   }
+  });
+		
+  function autoplay1(){
+   if(!is_animate1){
+    is_animate1 = true;
+    slideWrap1.animate({left: scrollSlider1}, 500, function(){
+     slideWrap1
+      .find('.slide3:first')
+      .appendTo(slideWrap)
+      .parent()
+      .css({'left': 0});
+     is_animate1 = false;
+    });
+   }
+  }
+ }		
+
+ htmSlider1();
+});
+		
